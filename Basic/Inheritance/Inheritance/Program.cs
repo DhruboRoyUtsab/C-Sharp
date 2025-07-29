094be70c208bb13b8000817ac8bf2dd5cb30ed5e
Dhrubo Roy <160015128+DhruboRoyUtsab@users.noreply.github.com>
2025-07-29 10:54:14 -0700

    Add files via upload

---
+++ Basic/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.MakeSound();
+            //myDog.Eat();
+
+            Cat myCat = new Cat();
+            myCat.MakeSound();
+        }
+    }
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Make sound");
+        }
+    }
+
+        class Dog : Animal
+        {
+            public override void MakeSound()
+            {
+                base.MakeSound();
+                Console.WriteLine("Barking");
+
+            }
+        }
+
+        class Collie : Dog
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Collie going Nuts");
+
+            }
+        }
+
+        class Cat : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Cat is Meowing");
+            }
+        }
+    }
