6dcd7ad9e3d7898f6d94531be34c8dd8f02943f3
Dhrubo Roy <160015128+DhruboRoyUtsab@users.noreply.github.com>
2025-07-30 10:36:04 -0700

    Add files via upload

---
+++ Basic/Interface/Interface/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Interface
+{
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(decimal amount);
+    }
+
+    public class CreditCardProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing credit card payment of {amount}");
+        }
+    }
+
+    public class PayPalProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing PayPal payment of {amount}");
+        }
+    }
+
+    public class PaymentService
+    {
+        private readonly IPaymentProcessor _processor;
+
+        public PaymentService(IPaymentProcessor processor)
+        {
+            _processor = processor;
+        }
+
+        public void ProcessOrderPayment(decimal amount)
+        {
+            _processor.ProcessPayment(amount);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPaymentProcessor creditCardProcessor = new CreditCardProcessor();
+            PaymentService paymentService = new PaymentService(creditCardProcessor);
+            paymentService.ProcessOrderPayment(100.00m);
+
+            IPaymentProcessor paypalProccesor = new PayPalProcessor();
+            paymentService = new PaymentService((paypalProccesor));
+            paymentService.ProcessOrderPayment(200.00m);
+        }
+    }
+}
