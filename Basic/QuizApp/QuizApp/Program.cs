cc8b78434d26ba047fed37f575340917fd6e9d9f
Dhrubo Roy <160015128+DhruboRoyUtsab@users.noreply.github.com>
2025-08-13 10:39:51 -0700

    Add files via upload

---
+++ Basic/QuizApp/QuizApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace QuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add Questions Here
+            Question[] questions = new Question[]
+            {
+                new Question
+                (
+                    "What is the capital of Germany?",
+                    new string[] { "Paris", "London", "Madrid","Berlin" },
+                    3
+                ),
+                new Question
+                (
+                    "What is 3*8+5?",
+                    new string[] {"29", "22", "39", "13" },
+                    0
+                ),
+                new Question
+                (
+                    "What is 2*11+1 ?",
+                    new string[] { "22", "24", "23", "12" },
+                    2
+                ),
+
+            };
+
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.StartQuiz();
+        }
+    }
+
+}
