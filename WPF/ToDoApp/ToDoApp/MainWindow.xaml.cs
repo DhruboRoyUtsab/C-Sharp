87e7a00b3cb7faa106c7e45ccf6c77ee4bc9461d
Dhrubo Roy <160015128+DhruboRoyUtsab@users.noreply.github.com>
2025-09-13 12:04:19 +0600

    Add files via upload

---
+++ WPF/ToDoApp/ToDoApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddToDoBtn(object sender, RoutedEventArgs e)
+        {
+            string todoText = ToDoInput.Text;
+
+            if (!string.IsNullOrEmpty(todoText))
+            {
+                TextBlock ToDoItem = new TextBlock
+                {
+                    Text = todoText,
+                    Margin = new Thickness(10)
+                };
+                ToDoList.Children.Add(ToDoItem);
+                ToDoInput.Clear();
+            }
+            else
+            {
+                MessageBox.Show("⚠️Empty!");
+            }
+        }
+    }
+}
